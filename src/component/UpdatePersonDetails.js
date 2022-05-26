94a5f7a3d60dc7b12cbf3524023f8997a49c4ff7
foxwayne1 <fox.wayne1@gmail.com>
2022-05-26 12:12:34 +0100

    details are being saved in state

--- src/component/UpdatePersonDetails.js
+++ src/component/UpdatePersonDetails.js
@@ -2,18 +2,48 @@ import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const UpdatePersonDetails = () => {
+  //   const paramDetails = useParams()
+  //   console.log(paramDetails)
+
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [isEnabled, setIsEnabled] = useState(false)
   const [isValid, setIsValid] = useState(false)
   const [isAuthorised, setIsAuthorised] = useState(false)
   const [favSport, setFavSport] = useState([{}])
+  const [allDetails, setAllDetails] = useState(null)
 
-  const paramDetails = useParams()
-  console.log(paramDetails)
+  const handleSubmit = e => {
+    e.preventDefault()
+    return setAllDetails({
+      firstName,
+      lastName,
+      isEnabled,
+      isValid,
+      isAuthorised,
+    })
+  }
+  console.log(allDetails)
+
+  const enableHandler = () => {
+    setIsEnabled(prevState => {
+      return (prevState = !prevState)
+    })
+  }
+  const validHandler = () => {
+    setIsValid(prevState => {
+      return (prevState = !prevState)
+    })
+  }
+  const authHandler = () => {
+    setIsAuthorised(prevState => {
+      return (prevState = !prevState)
+    })
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={e => handleSubmit(e)}>
         <label
           htmlFor='firstName'
           className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
@@ -29,98 +59,70 @@ const UpdatePersonDetails = () => {
           value={firstName}
           onChange={e => setFirstName(e.target.value)}
         />
+        <label
+          htmlFor='lastName'
+          className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+        >
+          Last Name
+        </label>
+        <input
+          type='text'
+          id='firstName'
+          aria-describedby='helper-text-explanation'
+          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          placeholder='Your First Name'
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
 
-        <fieldset>
-          <legend className='sr-only'>Checkbox variants</legend>
+        <div className='flex items-center mb-4'>
+          <input
+            id='enabled'
+            type='checkbox'
+            value={isEnabled}
+            onChange={enableHandler}
+            className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+          />
+          <label
+            htmlFor='enabled'
+            className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+          >
+            Enabled
+          </label>
+        </div>
 
-          <div className='flex items-center mb-4'>
-            <input
-              checked
-              id='checkbox-1'
-              type='checkbox'
-              value=''
-              className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-            />
-            <label
-              htmlFor='checkbox-1'
-              className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-            >
-              I agree to the
-            </label>
-          </div>
+        <div className='flex items-center mb-4'>
+          <input
+            id='valid'
+            type='checkbox'
+            value={isValid}
+            className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+            onChange={validHandler}
+          />
+          <label
+            htmlFor='valid'
+            className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+          >
+            Valid
+          </label>
+        </div>
 
-          <div className='flex items-center mb-4'>
-            <input
-              id='checkbox-2'
-              type='checkbox'
-              value=''
-              className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-            />
-            <label
-              htmlFor='checkbox-2'
-              className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-            >
-              I want to get promotional offers
-            </label>
-          </div>
-
-          <div className='flex items-center mb-4'>
-            <input
-              id='checkbox-3'
-              type='checkbox'
-              value=''
-              className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-            />
-            <label
-              htmlFor='checkbox-3'
-              className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-            >
-              I am 18 years or older
-            </label>
-          </div>
-
-          <div className='flex mb-4'>
-            <div className='flex items-center h-5'>
-              <input
-                id='helper-checkbox'
-                aria-describedby='helper-checkbox-text'
-                type='checkbox'
-                value=''
-                className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
-              />
-            </div>
-            <div className='ml-2 text-sm'>
-              <label
-                htmlFor='helper-checkbox'
-                className='font-medium text-gray-900 dark:text-gray-300'
-              >
-                Free shipping via Flowbite
-              </label>
-              <p
-                id='helper-checkbox-text'
-                className='text-xs font-normal text-gray-500 dark:text-gray-300'
-              >
-                For orders shipped from $25 in books or $29 in other categories
-              </p>
-            </div>
-          </div>
-
-          <div className='flex items-center'>
-            <input
-              id='international-shipping-disabled'
-              type='checkbox'
-              value=''
-              className='w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800'
-              disabled
-            />
-            <label
-              htmlFor='international-shipping-disabled'
-              className='ml-2 text-sm font-medium text-gray-400 dark:text-gray-500'
-            >
-              Eligible for international shipping (disabled)
-            </label>
-          </div>
-        </fieldset>
+        <div className='flex items-center mb-4'>
+          <input
+            id='authorised'
+            type='checkbox'
+            value='isAuthorised'
+            className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+            onChange={authHandler}
+          />
+          <label
+            htmlFor='authorised'
+            className='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+          >
+            authorised
+          </label>
+        </div>
+        <button type='submit'>Done</button>
       </form>
     </>
   )

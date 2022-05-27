33babe94408e556f3a155be6d4f98f54106ecde3
foxwayne1 <fox.wayne1@gmail.com>
2022-05-27 13:22:22 +0100

    done

--- src/component/UpdatePersonForm.js
+++ src/component/UpdatePersonForm.js
@@ -2,18 +2,16 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const UpdatePersonForm = ({ person }) => {
-  // console.log(person)
   let navigate = useNavigate()
 
   const [newPerson, setNewPerson] = useState(person)
-  const [firstName, setFirstName] = useState(person.firstName)
-  const [lastName, setLastName] = useState(person.lastName)
-  const [isEnabled, setIsEnabled] = useState(person.isEnabled)
-  const [isValid, setIsValid] = useState(person.isValid)
+  const [firstName, setFirstName] = useState(newPerson.firstName)
+  const [lastName, setLastName] = useState(newPerson.lastName)
+  const [isEnabled, setIsEnabled] = useState(newPerson.isEnabled)
+  const [isValid, setIsValid] = useState(newPerson.isValid)
   const [isAuthorised, setIsAuthorised] = useState(person.isAuthorised)
   const [isPalindrome, setIsPalindrome] = useState(person.isPalindrome)
 
-  console.log(newPerson)
   const handleCancel = () => {
     navigate('/', { replace: true })
   }
@@ -37,7 +35,7 @@ const UpdatePersonForm = ({ person }) => {
       <form onSubmit={e => handleSubmit(e)}>
         <label
           htmlFor='firstName'
-          className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+          className='block text-sm font-medium text-gray-900 dark:text-gray-300'
         >
           First Name
         </label>
@@ -45,7 +43,7 @@ const UpdatePersonForm = ({ person }) => {
           type='text'
           id='firstName'
           aria-describedby='helper-text-explanation'
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5'
+          className='inline bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5'
           placeholder='Your First Name Goes Here'
           value={firstName}
           onChange={e => setFirstName(e.target.value)}
